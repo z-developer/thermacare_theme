c0c8adaa44e15d423a3c217bbf67151017e3d639
z-developer <arman.zakaryan@gmail.com>
2015-04-04 11:10:48 +0400

    4

--- thermacare/templates/html.tpl.php
+++ thermacare/templates/html.tpl.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <head>
+<?php $themepath = 'https://thermacare4.pfizer.edrupalgardens.com/sites/g/files/g10021546/themes/site/thermacare/'; ?>
+
 <?php print $head; ?>
-<link href="themes/thermacare/css/thermacare.css" rel="stylesheet" type="text/css"/>
+<link href="<?=$themepath?>thermacare.css" rel="stylesheet" type="text/css"/>
  <!--A1-->
-<script type="text/javascript" src="themes/thermacare/js/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="<?=$themepath?>js/jquery-1.11.2.min.js"></script>
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-<script type="text/javascript" src="themes/thermacare/js/jquery.mCustomScrollbar.concat.min.js"></script>
-<script src="themes/thermacare/js/jquery.tools.min.js"></script>
-<script src="themes/thermacare/js/main.js"></script>
+<script type="text/javascript" src="<?=$themepath?>js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="<?=$themepath?>js/jquery.tools.min.js"></script>
+<script src="<?=$themepath?>js/main.js"></script>
 
 <title><?php print $head_title; ?></title>
 <?php print $styles; ?>
