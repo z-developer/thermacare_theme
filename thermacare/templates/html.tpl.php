07b11a6f0061da9fa4d2f73378beb46695722450
z-developer <arman.zakaryan@gmail.com>
2015-04-04 21:44:42 +0400

    5

--- thermacare/templates/html.tpl.php
+++ thermacare/templates/html.tpl.php
@@ -3,10 +3,13 @@
 <?php $themepath = 'https://thermacare4.pfizer.edrupalgardens.com/sites/g/files/g10021546/themes/site/thermacare/'; ?>
 
 <?php print $head; ?>
-<link href="<?=$themepath?>thermacare.css" rel="stylesheet" type="text/css"/>
- <!--A2-->
+<link href="<?=$themepath?>css/style.css" rel="stylesheet" type="text/css"/>
+
+ <!--A3-->
 <script type="text/javascript" src="<?=$themepath?>js/jquery-1.11.2.min.js"></script>
+<!--
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+-->
 <script type="text/javascript" src="<?=$themepath?>js/jquery.mCustomScrollbar.concat.min.js"></script>
 <script src="<?=$themepath?>js/jquery.tools.min.js"></script>
 <script src="<?=$themepath?>js/main.js"></script>
