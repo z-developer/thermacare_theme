37f5aac7649e01976cb30964b69c6d628cecc6b2
z-developer <arman.zakaryan@gmail.com>
2015-04-16 14:05:47 +0400

    A23

--- thermacare/templates/html.tpl.php
+++ thermacare/templates/html.tpl.php
@@ -5,7 +5,7 @@
     $themepath = '/'.$directory.'/';
 ?>
 
-<?php $ver='?a22'; ?>
+<?php $ver='?a23'; ?>
 
 <?php print $head; ?>
 <link href="<?=$themepath?>css/style.css<?=$ver?>" rel="stylesheet" type="text/css"/>
@@ -14,7 +14,7 @@
     .content > h2 { display: none;}
 </style>
 
- <!--A22-->
+ <!--A23-->
 <script type="text/javascript" src="<?=$themepath?>js/jquery-1.11.2.min.js<?=$ver?>"></script>
 <!--
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
